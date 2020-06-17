92f666d6bf752a4ea46094a5913c816e49d2c501
Hugo Flores Perez <hugofloresperez@Hugos-MacBook-Pro.local>
2020-06-17 10:36:52 -0400

    Different parse test

--- XMLParsingTest/ViewController.swift
+++ XMLParsingTest/ViewController.swift
@@ -13,6 +13,11 @@ struct Book {
     var bookAuthor: String
 }
 
+struct Part {
+    var name: String
+    var type: String
+}
+
 class ViewController: UITableViewController {
     
     var books: [Book] = []
@@ -20,13 +25,17 @@ class ViewController: UITableViewController {
     var bookTitle = String()
     var bookAuthor = String()
 
+    var parts: [Part] = []
+    var partName = String()
+    var partType = String()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         loadXMLFile()
     }
     
     func loadXMLFile() {
-        if let path = Bundle.main.url(forResource: "Books", withExtension: "xml") {
+        if let path = Bundle.main.url(forResource: "part_pv", withExtension: "xml") {
             if let parser = XMLParser(contentsOf: path) {
                 parser.delegate = self
                 parser.parse()
@@ -39,16 +48,17 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return books.count
+         return parts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
             
-        let book = books[indexPath.row]
+        // let book = books[indexPath.row]
+        let part = parts[indexPath.row]
             
-        cell.textLabel?.text = book.bookTitle
-        cell.detailTextLabel?.text = book.bookAuthor
+        cell.textLabel?.text = part.name
+        cell.detailTextLabel?.text = part.type
 
         return cell
     }
@@ -56,28 +66,56 @@ class ViewController: UITableViewController {
 
 extension ViewController: XMLParserDelegate {
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        if elementName == "book" {
-            bookTitle = String()
-            bookAuthor = String()
+        if elementName == "T" {
+            partName = String()
+            partType = String()
         }
         self.elementName = elementName
     }
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        if elementName == "book" {
-            let book = Book(bookTitle: bookTitle, bookAuthor: bookAuthor)
-            books.append(book)
+        if elementName == "T" {
+            let part = Part(name: partName, type: partType)
+            parts.append(part)
         }
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         if (!data.isEmpty) {
-            if self.elementName == "title" {
-                bookTitle += data
-            } else if self.elementName == "author" {
-                bookAuthor += data
+            if self.elementName == "P_NAME" {
+                partName += data
+            } else if self.elementName == "P_TYPE" {
+                partType += data
             }
         }
     }
 }
+
+//extension ViewController: XMLParserDelegate {
+//    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+//        if elementName == "book" {
+//            bookTitle = String()
+//            bookAuthor = String()
+//        }
+//        self.elementName = elementName
+//    }
+//
+//    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+//        if elementName == "book" {
+//            let book = Book(bookTitle: bookTitle, bookAuthor: bookAuthor)
+//            books.append(book)
+//        }
+//    }
+//
+//    func parser(_ parser: XMLParser, foundCharacters string: String) {
+//        let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+//        if (!data.isEmpty) {
+//            if self.elementName == "title" {
+//                bookTitle += data
+//            } else if self.elementName == "author" {
+//                bookAuthor += data
+//            }
+//        }
+//    }
+//}
